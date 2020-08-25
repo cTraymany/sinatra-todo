04447014127d9c03b325c7c11c0d65cf78d72b96
cTraymany <chindalatht@gmail.com>
2020-08-25 19:40:49 +0700

    redirects tasks if not logged in

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,14 @@ class TasksController < ApplicationController
 
   # GET: /tasks
   get "/tasks" do
+    redirect_if_not_logged_in
+    # there is no task/index erb
     erb :"/tasks/index"
   end
 
   # GET: /tasks/new
   get "/tasks/new" do
+    redirect_if_not_logged_in
     erb :"/tasks/new"
   end
 
@@ -22,12 +25,15 @@ class TasksController < ApplicationController
 
   # GET: /tasks/5
   get "/tasks/:id" do
+    redirect_if_not_logged_in
+    # task show page -- maybe delete this one
     # binding.pry
     erb :"/tasks/show"
   end
 
   # GET: /tasks/5/edit
   get "/tasks/:id/edit" do
+    redirect_if_not_logged_in
     @task = Task.find_by(id:params[:id])
     erb :"/tasks/edit"
   end
